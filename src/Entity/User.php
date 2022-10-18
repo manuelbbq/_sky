b8f51882569383b649f8811f6bce96e580d3494d
manuelbbq <man.martinez@bbqberlin.onmicrosoft.com>
2022-10-18 15:40:32 +0200

    add register

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface ,PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -40,6 +42,17 @@ class User implements UserInterface ,PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     private ?string $plainPassword;
+
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+
+
+
+
 
     public function getId(): ?int
     {
